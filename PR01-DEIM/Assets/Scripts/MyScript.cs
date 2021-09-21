8dd2cf6c8915a128cd20c125f3dc6c0d9ed62be1
IvanGallego <>
2021-09-21 13:07:25 +0200

    commit actividad 3.c
    Commit con la creación de una corrutina para crear un contador

--- PR01-DEIM/Assets/Scripts/MyScript.cs
+++ PR01-DEIM/Assets/Scripts/MyScript.cs
@@ -6,15 +6,12 @@ public class MyScript : MonoBehaviour
 {
 
     //Mis variables
-    [SerializeField] int num1; 
-    [SerializeField] int num2; 
-    [SerializeField] int operador;
-    int resultado;
+    private int num = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Calcular(); 
+        StartCoroutine("Contador");
     }
 
     // Update is called once per frame
@@ -23,30 +20,14 @@ public class MyScript : MonoBehaviour
        
     }
 
-    // Método de saludo
-    void Calcular()
+   IEnumerator Contador()
     {
-        //Si, se puede hacer con un switch, pero nunca se me dieron bien :)
-        if (operador == 0)
+        while (num <= 10)
         {
-            resultado = num1 + num2;
-        }
-        else if (operador == 1)
-        {
-            resultado = num1 - num2;
-        }
-        else if (operador == 2)
-        {
-            resultado = num1 * num2;
-        }
-        else if (operador == 3)
-        {
-            resultado = num1 / num2;
-        }
-        else
-        {
-            print("Introduce un valor entre 0 y 3, ambos incluidos");
-        }
-        print(resultado);
+            yield return new WaitForSeconds(1f);
+            print(num);
+            num++;
+        };
     }
+    
 }
