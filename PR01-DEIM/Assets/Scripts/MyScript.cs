e8a8e0191f80f4c4b70a04f7eb9e2e95e1edd4e7
IvanGallego <>
2021-09-21 12:18:54 +0200

    commit actividad 3.a propuesta Álvaro
    Crear un método para que devuelva un saludo según el método de Álvaro

--- PR01-DEIM/Assets/Scripts/MyScript.cs
+++ PR01-DEIM/Assets/Scripts/MyScript.cs
@@ -6,28 +6,23 @@ public class MyScript : MonoBehaviour
 {
 
     //Mis variables
-    private int n = 0;
+    [SerializeField] string nombre = "personita anónima";
     
     // Start is called before the first frame update
     void Start()
     {
-        
-        while (n != 5)
-        {
-
-            print("El valor es: " + n + ". Continua!");
-            n = Random.Range(0, 1001);
-
-        }
-
-        print("Ok, el valor es 5, pues me paro");
-
-
+        Saludo(nombre);
     }
 
     // Update is called once per frame
     void Update()
     {
        
+    }
+
+    // Método de saludo
+    void Saludo(string name)
+    {
+        print ("Hola " + name);
     }
 }
