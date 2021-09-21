ca115ba283a07c80a8fe20b79d55d7b9175f46b0
IvanGallego <>
2021-09-21 10:59:52 +0200

    commit actividad 2.c
    Actividad de creación de bucles con estructuras de control para enviar distintos mensajes

--- PR01-DEIM/Assets/Scripts/MyScript.cs
+++ PR01-DEIM/Assets/Scripts/MyScript.cs
@@ -7,20 +7,29 @@ public class MyScript : MonoBehaviour
 
     //Mis variables
     private int numWhile = 0;
-    private int numFor;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        while (numWhile < 10)
+        while (numWhile <= 10)
         {
+
+            if (numWhile < 5)
+            {
+                print(numWhile + " es menor que 5");
+            }
+            else if (numWhile == 5) 
+            {
+                print(numWhile);
+            }                            
+            else //if (numWhile >5)
+            {
+                print(numWhile + " es mayor que 5");
+            };
+
             numWhile++;
-            print("El número actual del bucle while es: " + numWhile);
         };
-        for (numFor = 10; numFor >= 0; numFor--)
-        {
-            print("El resultado del bucle for es: " + numFor);
-        };
+        
             
     }
 
