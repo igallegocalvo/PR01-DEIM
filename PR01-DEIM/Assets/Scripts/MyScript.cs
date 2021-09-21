2494703a5963f5e20e1772e74140d15e3b15ed16
IvanGallego <>
2021-09-21 10:24:42 +0200

    commit actividad 2.b
    Commit conbucles for y while en el método Start

--- PR01-DEIM/Assets/Scripts/MyScript.cs
+++ PR01-DEIM/Assets/Scripts/MyScript.cs
@@ -6,22 +6,27 @@ public class MyScript : MonoBehaviour
 {
 
     //Mis variables
-    private int init = 0;
-    
+    private int numWhile = 0;
+    private int numFor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        while (numWhile < 10)
+        {
+            numWhile++;
+            print("El número actual del bucle while es: " + numWhile);
+        };
+        for (numFor = 10; numFor >= 0; numFor--)
+        {
+            print("El resultado del bucle for es: " + numFor);
+        };
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (init < 10)
-        {
-            init++;
-            print(init);
-        }
+       
     }
 }
