6df1634050bdca3c6380938b475da2d723d70a23
IvanGallego <>
2021-09-21 09:53:59 +0200

    commit actividad 2.a
    Creación de un bucle en el método Update que llegue a 10 y se detenga

--- PR01-DEIM/Assets/Scripts/MyScript.cs
+++ PR01-DEIM/Assets/Scripts/MyScript.cs
@@ -6,22 +6,22 @@ public class MyScript : MonoBehaviour
 {
 
     //Mis variables
-
-    private string saludo = "Hola ";
-    [SerializeField] string nombre = "personita anónima";
-    public int num1 = 5;
-    public int num2 = 2;
+    private int init = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        print(saludo + nombre + ", el resultado de multiplicar " + num1 + " y " + num2 +" es:");
-        print(num1 * num2);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (init < 10)
+        {
+            init++;
+            print(init);
+        }
     }
 }
